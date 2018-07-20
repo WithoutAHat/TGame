8c2f11a7e26f78b4a598f68d8d8af8347ddf9976
DESKTOP-P5FKVCR\Appa <mathieu.r.lachance@gmail.com>
2018-07-19 20:19:16 -0400

    Changed basic functionality of the boundary system.

---
+++ Assets/Scripts/GameMapHandler.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using System.Linq;
+
+public class GameMapHandler : MonoBehaviour {
+
+    private int[] newOccupied, oldOccupied;
+
+    // Use this for initialization
+    void Start () {
+        MakeIntoPlusStyleBoundaries();
+        CheckForClear();
+
+
+        DebugLogNewOccupied();
+        DebugLogOldOccupied();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    // Initializes Occupied arrays with + style boundaries
+    private void MakeIntoPlusStyleBoundaries() {
+        // Eventually add some code to reset the map and boundaries
+        // Destroying all blocks
+
+        newOccupied = new int[36 * 36];
+        oldOccupied = new int[36 * 36];
+
+        // Remove all blocks from Occupied
+        for (int i = 0; i < 36 * 36; i++) 
+            newOccupied[i] = 0;
+
+        // Set non-removable blocks in Occupied
+        for (int i = 0; i < 13; i++) {
+            // Top 2 lines
+            newOccupied[i * 36 + 12] = 2;
+            newOccupied[i * 36 + 23] = 2;
+            // Bottom 2 lines
+            newOccupied[(i + 23) * 36 + 12] = 2;
+            newOccupied[(i + 23) * 36 + 23] = 2;
+
+            // Left 2 Lines
+            newOccupied[12 * 36 + i] = 2;
+            newOccupied[12 * 36 + i + 23] = 2;
+            // Right 2 Lines
+            newOccupied[23 * 36 + i] = 2;
+            newOccupied[23 * 36 + i + 23] = 2;
+        }
+        // Middle 2x2 square
+        newOccupied[18 * 36 + 18] = 2;
+        newOccupied[18 * 36 + 19] = 2;
+        newOccupied[19 * 36 + 18] = 2;
+        newOccupied[19 * 36 + 19] = 2;
+
+        // Make oldOccupied the same as newOccupied
+        for (int i = 0; i < 36 * 36; i++) 
+            oldOccupied[i] = newOccupied[i];
+        
+    }
+
+    // Checks for a clearing area
+    private void CheckForClear() {
+        for (int i = 0; i < 36 * 36; i++) {
+            // Check for filled 4*4 areas
+            // Then clear them
+        }
+    }
+
+    // Use to create a block at Vector2 coordinate
+    private void CreateBlockAtBoundary(Vector2 coordinate) {
+
+    }
+
+    // Use to create a block at int coordinate
+    private void CreateBlockAtBoundary(int coordinate) {
+
+    }
+    
+    // Use to set a single block as a boundary
+    public void SetBoundary (Vector2 v) {
+        // remove the vector coords decimals or make them into 0.5s as necessary 
+    }
+
+    // Use to add to the Occupied Array blocks that can be removed
+    public void AddToOccupied(Vector2[] toMakeOccupied) {
+
+
+    }
+
+    // Use to print a debug of newOccupied
+    public void DebugLogNewOccupied () {
+        string line = "";
+        for (int i = 0; i < 36; i++) {
+            for (int j = 0; j < 36; j++) {
+                line += newOccupied[i * 36 + j].ToString();
+            }
+            line += "\n";
+        }
+        Debug.Log(line);
+    }
+
+    // Use to print a debug of oldOccupied
+    public void DebugLogOldOccupied() {
+        string line = "";
+        for (int i = 0; i < 36; i++) {
+            for (int j = 0; j < 36; j++) {
+                line += oldOccupied[i * 36 + j].ToString();
+            }
+            line += "\n";
+        }
+        Debug.Log(line);
+    }
+}
--- Assets/Scripts/TetrominoHandler.cs
+++ Assets/Scripts/TetrominoHandler.cs
@@ -6,14 +6,15 @@ public class TetrominoHandler : MonoBehaviour {
 
     float fall = 0;
     public float fallSpeed = 1;
-    private GridHandler gameGrid;
+    private GameMapHandler gameGrid;
     public bool allowRotation = true;
     bool limitRotation = false;
+    private Vector2[] blockPositions;
 
     // Use this for initialization
     void Start () {
-        gameGrid = GameObject.Find("Grid").GetComponent<GridHandler>();
-
+        gameGrid = GameObject.Find("Grid").GetComponent<GameMapHandler>();
+        BlockPositions();
     }
 	
 	// Update is called once per frame
@@ -25,52 +26,65 @@ public class TetrominoHandler : MonoBehaviour {
     void CheckUserInput() {
         if (Input.GetKeyDown(KeyCode.RightArrow)) {
             transform.position += new Vector3(1, 0, 0);
-            if (!CheckIsValidPosition())
-                transform.position += new Vector3(-1, 0, 0);
+            //if (!CheckIsValidPosition())
+            //    transform.position += new Vector3(-1, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
             transform.position += new Vector3(-1, 0, 0);
-            if (!CheckIsValidPosition())
-                transform.position += new Vector3(1, 0, 0);
+            //if (!CheckIsValidPosition())
+            //    transform.position += new Vector3(1, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow)) {
             if (allowRotation) { //look this over for improvement
                 if (limitRotation) {
                     if (transform.rotation.z == 0) {
                         transform.Rotate(0, 0, 90);
-                        if (!CheckIsValidPosition())
-                            transform.Rotate(0, 0, -90);
+                        //if (!CheckIsValidPosition())
+                        //    transform.Rotate(0, 0, -90);
                     }
                     else
                         transform.Rotate(0, 0, -90);
-                        if (!CheckIsValidPosition())
-                            transform.Rotate(0, 0, 90);
+                    //if (!CheckIsValidPosition())
+                    //    transform.Rotate(0, 0, 90);
                 }
                 else {
                     transform.Rotate(0, 0, 90);
-                    if (!CheckIsValidPosition())
-                        transform.Rotate(0, 0, -90);
-                } 
+                    //if (!CheckIsValidPosition())
+                    //    transform.Rotate(0, 0, -90);
+                }
             }
         }
 
         // Fall speed determined by fallSpeed from 1 block/s to 1 block/0.05s
         // Fall by 1 per DownArrow key press
         // Fall disabled if fallSpeed = 0
-        else if (fallSpeed != 0 && (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= (21 - fallSpeed * 2)/20)) {
+        else if (fallSpeed != 0 && (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= (21 - fallSpeed * 2) / 20)) {
             transform.position += new Vector3(0, -1, 0);
             fall = Time.time;
-            if (!CheckIsValidPosition()) {
-                transform.position += new Vector3(0, 1, 0);
-            }    
+            //if (!CheckIsValidPosition()) {
+            //    transform.position += new Vector3(0, 1, 0);
+            //}    
         }
     }
-    private bool CheckIsValidPosition() {
-        bool valid = true;
-        foreach (Transform mino in transform) {
-            if (!gameGrid.CheckIsInsideGrid(mino.position))
-                valid = false;
+    private bool CheckIsValidPosition() { 
+        // take tetromino position and add mino position an convert that into index to check validity
+
+        if (true) {
+            return false;
         }
-        return valid;
-    } 
+        else
+            return true;
+    }
+
+    // Store vector information for block positions in blockPositions array
+    private void BlockPositions() {
+        blockPositions = new Vector2[4];
+        for (int i = 0; i < transform.childCount; i++) {
+            Transform t = transform.GetChild(i);
+            if (t.gameObject.tag == "Mino") {
+                Vector2 v = new Vector2(t.localPosition.x, t.localPosition.y);
+                blockPositions[i] = v;
+            }
+        }
+    }
 }
